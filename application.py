59e9ea67e3914ace35a45066d3f739fe28d65f82
Arushi <arushi.arora@columbia.edu>
2016-05-12 17:14:23 -0400

    Rotten Tomato rating

--- application.py
+++ application.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask, render_template, request, redirect, url_for, send_from_directory, json, jsonify
 import omdb
+import math
 
 app = Flask(__name__)
 
@@ -14,14 +15,24 @@ def search():
 	print 'hello'
 	term = request.form['keyword']
 	print term
-	res = omdb.get(title=term)
+	res = omdb.get(title=term, tomatoes=True)
 	imdb_rating = res.imdb_rating
+	term = res.title
 	plot = res.plot
-	overall_rating = 8
-	youtube_rating = 4
-	twitter_rating = 6
-	return render_template('ratings.html', keyword=term, plot=plot, imdb_rating=imdb_rating, twitter_rating=twitter_rating, \
-		youtube_rating=youtube_rating, overall_rating=overall_rating)
+	tomato_rating = res.tomato_rating
+	overall_rating = 8.9
+	youtube_rating = 4.6
+	twitter_rating = 6.3
+	if (tomato_rating=="N/A"):
+		tomato_rating = tomato_rating.replace("/", "")
+	overall_img = "Images/" + str(math.trunc(overall_rating)) + ".png"
+	twitter_img = "Images/" + str(math.trunc(twitter_rating)) + ".png"
+	youtube_img = "Images/" + str(math.trunc(youtube_rating)) + ".png"
+	imdb_img = "Images/" + imdb_rating.split('.', 1)[0] + ".png"
+	tomato_img = "Images/" + tomato_rating.split('.', 1)[0] + ".png"
+	return render_template('ratings.html', keyword=term, plot=plot, imdb_rating=imdb_rating, youtube_rating=youtube_rating, \
+		tomato_rating=tomato_rating, twitter_rating=twitter_rating, overall_rating=overall_rating, overall_img=overall_img, \
+		youtube_img=youtube_img, twitter_img=twitter_img, imdb_img=imdb_img, tomato_img=tomato_img)
 
 if __name__ == '__main__':
     app.run(
